ca361473446e2f3d2edc388b45a77d8416e601a4
Antoni Martyniuk <a.martyniuk@convertiser.com>
2019-05-22 19:19:19 +0200

    unwraps known object types

--- src/ElasticSearch/Parameters/Factory/SearchFactory.php
+++ src/ElasticSearch/Parameters/Factory/SearchFactory.php
@@ -3,7 +3,7 @@
 namespace Drupal\elasticsearch_connector\ElasticSearch\Parameters\Factory;
 
 use Drupal\elasticsearch_connector\ElasticSearch\Parameters\Builder\SearchBuilder;
-use Drupal\search_api\Item\FieldInterface;
+use Drupal\elasticsearch_connector\Utility\Utility;
 use Drupal\search_api\Query\QueryInterface;
 
 /**
@@ -57,12 +57,34 @@ class SearchFactory {
 
         // Set each item in _source as a field in Search API.
         foreach ($result['_source'] as $elasticsearch_property_id => $elasticsearch_property) {
-          // Make everything a multifield.
-          if (!is_array($elasticsearch_property)) {
-            $elasticsearch_property = [$elasticsearch_property];
+          $is_assoc_property = is_array($elasticsearch_property) && Utility::isArrayAssoc($elasticsearch_property);
+
+          // Unwrap objects to flat fields.
+          $index_field = $index->getField($elasticsearch_property_id);
+          if ($is_assoc_property && $index_field !== NULL && $index_field->getType() === 'object') {
+            $flatten_fields = Utility::flattenArray($elasticsearch_property, $elasticsearch_property_id, '.');
+            foreach ($flatten_fields as $flatten_field_key => $flatten_field_value) {
+              $flat_field = $fields_helper->createField($index, $flatten_field_key, ['property_path' => $flatten_field_key]);
+
+              if (is_scalar($flatten_field_value) || (is_array($flatten_field_value) && Utility::isArrayAssoc($flatten_field_value))) {
+                $flat_field->addValue($flatten_field_value);
+              }
+              else {
+                $flat_field->setValues((array) $flatten_field_value);
+              }
+
+              $result_item->setField($flatten_field_key, $flat_field);
+            }
           }
+
           $field = $fields_helper->createField($index, $elasticsearch_property_id, ['property_path' => $elasticsearch_property_id]);
-          $field->setValues($elasticsearch_property);
+          if ($is_assoc_property) {
+            $field->addValue($elasticsearch_property);
+          }
+          else {
+            $field->setValues((array) $elasticsearch_property);
+          }
+
           $result_item->setField($elasticsearch_property_id, $field);
         }
 
